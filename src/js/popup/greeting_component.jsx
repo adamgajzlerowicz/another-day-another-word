3c2fb7ddd540e25fe5360f7b40e2a648b4cc9b57
Adam Gajzlerowicz <nelf86@gmail.com>
2018-03-31 12:48:38 +0200

    lint

--- src/js/popup/greeting_component.jsx
+++ src/js/popup/greeting_component.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import icon from "../../img/icon-128.png"
+import React from 'react';
+import icon from '../../img/icon-128.png';
 
 export default class extends React.Component {
-  render () {
-    return (
-      <div>
-        <p>Hello, find me on src/js/popup/greeting_component.jsx</p>
-        <img src={icon} />
-      </div>
-    )
-  }
-};
+    render () {
+        return (
+            <div>
+                <img src={icon} />
+            </div>
+        );
+    }
+}
+
