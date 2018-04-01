7f149999920d385e443249d6da50a531b3252534
adamgajzlerowicz <nelf86@gmail.com>
2018-04-01 20:12:49 +0200

    replace working

--- src/js/background.js
+++ src/js/background.js
@@ -1,47 +1,34 @@
 /* global chrome */
 
-const word = 'blah';
-// (function () {
-//     // create a new div element
-//     var newDiv = document.createElement('div');
-//     // and give it some content
-//     var newContent = document.createTextNode('Hi there and greetings!');
-//     // add the text node to the newly created div
-//     newDiv.appendChild(newContent);
-//     newDiv.style.background = 'red';
-//     // add the newly created element and its content into the DOM
-//     var currentDiv = document.getElementById('div1');
-//     document.body.insertBefore(newDiv, currentDiv);
-// })())
 const replaceText = (text, words) => {
     let newContent = text;
 
     Object.keys(words).map((word) => {
-        // newContent = newContent.replace(new RegExp(words[word].synonyms.join('|'), 'ig'), '<span style="color: red">dupa</span>');
+        newContent = newContent.replace(new RegExp(words[word].synonyms.join('|'), 'ig'), `*${word}*`);
     });
     return newContent;
 };
 
-const getChildNodes = (node, words) => {
+const update = (node, words) => {
     if (node.hasChildNodes()) {
         const children = node.childNodes;
 
         for (let i = 0; i < children.length; i++) {
-            getChildNodes(children[i], words);
+            update(children[i], words);
         }
 
     } else {
         if (node.textContent) {
-            console.log(node);
             node.textContent = replaceText(node.textContent, words);
         }
     }
 };
+
 window.onload = function () {
     chrome.storage.sync.get(['redux'], function (result) {
         if (result.redux) {
             const words = JSON.parse(result.redux).words;
-            words && getChildNodes(document.body, words);
+            words && update(document.body, words);
         }
     });
 };
